e351cc6a478ec7627804e47901c2d233e4a02014
Ivan <diestoapp@gmail.com>
2025-12-19 16:45:51 +0300

    homework 14.1 fix_2

--- src/product_category.py
+++ src/product_category.py
@@ -12,6 +12,7 @@ class Product:
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
+        """Инициализация продукта"""
         self.name = name
         self.description = description
         self.price = price
@@ -28,12 +29,15 @@ class Category:
     name: str
     description: str
     products: list[Product]
+
     category_count: int = 0
     product_count: int = 0
 
     def __init__(self, name: str, description: str, products: list[Product]) -> None:
+        """Инициализация категории"""
         self.name = name
         self.description = description
-        self.products = products
+        self.products = products if products else []
+
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += len(self.products)
